92e1b12f14efdfa66a81f869dd4b3346e953ab40
Geoff Woodburn <geoff@woodburndesigns.com>
2020-10-08 19:39:19 -0700

    Adding an add and remove user from organization step

---
+++ steps/user-remove/step.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# NPM credentials (required for commands like `publish`)
+NPM_TOKEN=$(ni get -p '{.npm.token}')
+ORGANIZATION=$(ni get -p '{.npm.organization}')
+USER=$(ni get -p '{.user}')
+ROLE=$(ni get -p '{.role}')
+[ "$ROLE" ] || ROLE="developer"
+
+# Create .npmrc with credentials
+if [ -n "${NPM_TOKEN}" ]; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ./.npmrc
+fi
+
+npm org rm $ORGANIZATION $USER
